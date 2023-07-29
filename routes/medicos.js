2c28f326c9d396b72b56d712ee5c4049648716b6
milton munoz <livemilton@hotmail.com>
2023-07-29 16:15:10 -0500

    Fin seccion 13

--- routes/medicos.js
+++ routes/medicos.js
@@ -31,11 +31,17 @@ router.post('/',
 );
 
 router.put( '/:id', 
-    [],
+    [
+        validarJWT,
+        check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
+        check('hospital','El hospital id debe de ser válido').isMongoId(),
+        validarCampos 
+    ],
     actualizarMedico
 );
 
 router.delete( '/:id',
+    validarJWT,
     borrarMedico
 );
 
